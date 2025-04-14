87c52e2a6c7ad690b4050c4c002cb3053fd3156c
dhroovpandey <dhroovpandey@my.unt.edu>
2025-04-14 13:52:57 -0500

    Initialized Login Page (no design)

---
+++ airport-management-frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home(){
+    return (
+        <div className="Home">
+        <header className="HomeHeader"><h1>Airport Home</h1></header>    
+        </div>
+    );
+}
