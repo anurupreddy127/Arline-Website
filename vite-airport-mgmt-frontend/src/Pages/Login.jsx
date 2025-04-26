03962ff4119a2f75acc06e237dfdfca8fa128f3f
Anurup Reddy <koduruanurupreddy5@gmail.com>
2025-04-25 22:57:11 -0500

    resolved a conflict

--- vite-airport-mgmt-frontend/src/Pages/Login.jsx
+++ vite-airport-mgmt-frontend/src/Pages/Login.jsx
@@ -27,16 +27,8 @@ function Login() {
       });
   };
 
-<<<<<<< HEAD
   const add_user_handler = () => {
-    let len = 0;
-    fetch("http://localhost:8080/api/users")
-      .then((data) => data.json())
-      .then((data) => {
-        len = data.length + 1;
-      });
     const postData = {
-      id: len,
       username: user.toString(),
       password: pwd.toString(),
       role: "PASSENGER",
@@ -63,6 +55,7 @@ function Login() {
         console.error("Error:", error);
       });
   };
+
   return (
     <div style={{ display: "flex", flexDirection: "row", minHeight: "100vh" }}>
       <div style={{ display: "flex", minHeight: "100vh", flex: 1 }}>
@@ -93,59 +86,8 @@ function Login() {
             value={user}
             onChange={(event) => setUser((p) => event.target.value)}
           />
-=======
-    const add_user_handler = ()=>{
-        let len = 0;
-        //fetch('http://localhost:8080/api/users').then((data)=>data.json()).then(data=>{len=data.length+1})
-        const postData = {
-            //id: len,
-            username: user.toString(),
-            password: pwd.toString(),
-            role: 'PASSENGER',
-        };
-        const options = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData)
-        };
-        fetch('http://localhost:8080/api/users', options)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
-    return (
-        <div style={{display: "flex", flexDirection: 'row', minHeight:'100vh'}}>
-        <div style={{display:'flex', minHeight: '100vh', flex:1}}><img src={login_img} style={{ flex:1}}/></div>
-        <div style={{flex:1, display:'flex', flexDirection:"column", alignItems:'center', padding:'40px', backgroundColor: 'beige'}}>
-                <div style={{display: 'flex', flexDirection: "column", padding: '10px', width: '90%'}}>
-                <label style={{fontSize:'28px'}}>
-                    User ID:
-                </label>
-                <input placeholder={"Username"} style={{border:'1px solid'}} value={user} onChange={(event)=>setUser((p)=>event.target.value)}/>
-                </div>
-                <div style={{display: 'flex', flexDirection: "column", padding:'10px', width: '90%'}}>
-                <label style={{fontSize:'28px'}}>
-                    Password:
-                </label>
-                <input placeholder={"Password"} style={{border: '1px solid', width: '100%'}} value={pwd} onChange={(text)=>setPwd((prev)=>text.target.value)}/>
-                </div>
-                <button onClick={()=>login_handler()} style={{fontSize:'20px', backgroundColor: 'lightgray', cursor: 'pointer', border: '1px solid', width: '50%'}}>Log In</button>
-                <button onClick={()=>add_user_handler()} style={{fontSize:'20px', backgroundColor: 'lightgray', cursor: 'pointer', border: '1px solid', width: '50%'}}>Add User</button>
-            </div>
->>>>>>> 10753260ee23bfa321cc7320487b92cb9fc5bcc7
         </div>
+
         <div
           style={{
             display: "flex",
@@ -162,6 +104,7 @@ function Login() {
             onChange={(text) => setPwd((prev) => text.target.value)}
           />
         </div>
+
         <button
           onClick={() => login_handler()}
           style={{
@@ -174,6 +117,7 @@ function Login() {
         >
           Log In
         </button>
+
         <button
           onClick={() => add_user_handler()}
           style={{
