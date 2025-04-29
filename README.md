6c2ef1caebef668ccf22a2005238bfe65edcace0
Anurup Reddy <koduruanurupreddy5@gmail.com>
2025-04-28 21:53:46 -0500

    Created and Completed the project

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# 5350Project
-This is the repository for the airport management system built as prototype 
+# ✈️ Airport Management System
+
+A full-stack Airport Passenger Management System designed to handle:
+
+# 📂 Project Structure
+
+Layer Technology
+Frontend React.js
+Backend Spring Boot (Java)
+Database MySQL
+
+# 🛠️ Technologies Used
+
+Frontend:
+React.js (Hooks, Fetch API)
+TailwindCSS (basic)
+
+Backend:
+Spring Boot
+Spring MVC + REST APIs
+JDBC Template
+
+Database:
+MySQL (Workbench)
+
+Version Control:
+Git + GitHub
+
+# 🏗️ Setup Instructions
+
+1. Clone the Repository
+
+2. Backend Setup (Spring Boot)
+   --> Make sure MySQL server is running.
+   --> Update your application.properties for DB credentials.
+   --> Open the backend folder in system terminal ro vscode terminal.
+   --> Run the Spring Boot Application using this command :
+   `mvn spring-boot:run`
+
+3. Frontend Setup (React.js)
+   --> Open the frontend folder in system terminal or vscode terminal
+   --> Install dependencies using this command :
+   `npm install`
+   --> Start the frontend server :
+   `npm run dev`
--- airport-mgmt-sys/src/main/java/com/airportmanagment/controller/TicketController.java
+++ airport-mgmt-sys/src/main/java/com/airportmanagment/controller/TicketController.java
@@ -1,16 +1,23 @@
 package com.airportmanagment.controller;
 
-import com.airportmanagment.model.Ticket;
-import com.airportmanagment.service.TicketService;
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
+import com.airportmanagment.model.Ticket;
+import com.airportmanagment.service.TicketService;
 
 @RestController
 @RequestMapping("/api/tickets")
